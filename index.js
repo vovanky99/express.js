7032b998bd28d93596ec5f9954c9bef0d7538c96
vovanky99 <150527055+vovanky99@users.noreply.github.com>
2024-03-14 22:24:51 +0700

    morgan

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+var morgan = require('morgan')
 const app = express();
 const port = 3000
+
+app.use(morgan('combined'))
 
 app.get('/',(req,res)=>{
     return res.send('Hello world!')
